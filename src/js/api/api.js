2f0c72ca28d004bcff45854277c1946b11ffd1ed
Andrii Hashenko <hashenkoaa@gmail.com>
2023-04-13 22:24:53 +0300

    add start files and API

---
+++ src/js/api/api.js
@@ -0,0 +1,7 @@
+const API_KEY = '900c3c2e823d6abe3929dac959f94e63';
+const BASE_URL = 'https://api.themoviedb.org/3';
+const TREND_URL = '${BASE_URL}/trending/movi';
+const SEARCH_URL = '${BASE_URL}/search/movi';
+const ID_URL = '${BASE_URL}/movi';
+
+export { API_KEY, BASE_URL, TREND_URL, SEARCH_URL, ID_URL };
