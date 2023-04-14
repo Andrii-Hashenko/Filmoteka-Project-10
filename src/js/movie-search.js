ae3a2aa0c4883efa2ca8446f9d1f0dff4349dee5
OlenkaPasko <lena_nechipurenko@ukr.net>
2023-04-15 01:09:37 +0300

    Update movie-search.js

--- src/js/movie-search.js
+++ src/js/movie-search.js
@@ -1,1 +1,15 @@
 //const SEARCH_URL = '${BASE_URL}/search/movi';
+
+//Function declaration
+//async function foo() {
+  // ...
+//}
+getMov(SEARCH_URL);
+//пошук фільмів
+async function getMov() { 
+
+}
+//відображення фільмів
+function showMov(){
+
+}
