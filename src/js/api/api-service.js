c32b53d72c851cb0321f8c40c521564463cfadf2
Andrii Hashenko <hashenkoaa@gmail.com>
2023-04-15 17:34:34 +0300

    update api

--- src/js/api/api-service.js
+++ src/js/api/api-service.js
@@ -1,43 +1,35 @@
-import axios from "axios";
+import axios from 'axios';
 import { API_KEY, BASE_URL, TREND_URL, ID_URL, ID_URL } from './api';
 
 export default {
-    async getTrendData(page) {
-        try {
-            const { data } = await axios.get(
-                `${TREND_URL}?api_key=${API_KEY}&page=${page}`,
-            );
-            return data;
-        } catch (error) {
-            console.error('Smth wrong with api get full trends' + error);
-        }
-    },
+  async getTrendData(page) {
+    try {
+      const { data } = await axios.get(
+        `${TREND_URL}?api_key=${API_KEY}&page=${page}`
+      );
+      return data;
+    } catch (error) {
+      console.error('Smth wrong with api get full trends' + error);
+    }
+  },
+};
+
+async function fetchMovieSearcher(text, page) {
+  try {
+    const { data } = await axios.get(
+      `${SEARCH_URL}?api_key=${API_KEY}&query=${text}&page=${page}`
+    );
+    return data;
+  } catch (error) {
+    console.error('Smth wrong with api search fetch' + error);
+  }
 }
 
-//     async fetchMovieSearcher(text, page) {
-//     try {
-//         const { data } = await axios.get (
-// `${SEARCH_URL}?api_key=${API_KEY}&query=${text}&page=${page}`
-//         );
-//         return data;
-//     }
-// catch (error) {
-//     console.error('Smth wrong with api search fetch' + error);
-// }
-
-// };
-
-// async getMovieById(id) {
-//     try {
-//         const { data } = await axios.get (
-// `${ID_URL}${id}?api_key=${API_KEY}`
-//         );
-//         const result = {
-           
-//         }
-//     }
-// catch (error) {
-//     console.error('Smth wrong with api ID fetch' + error);
-// }
-
-// };
+async function getMovieById(id) {
+  try {
+    const { data } = await axios.get(`${ID_URL}${id}?api_key=${API_KEY}`);
+    const result = {};
+  } catch (error) {
+    console.error('Smth wrong with api ID fetch' + error);
+  }
+}
