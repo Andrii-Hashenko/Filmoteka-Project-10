53ff48aafe064ac93ab21a0f34905ceb6d85a839
Andrii-Hashenko <113782851+Andrii-Hashenko@users.noreply.github.com>
2023-04-16 20:01:25 +0000

    Deploying to gh-pages from @ Andrii-Hashenko/Filmoteka-Project-10@f3ae75bbe70731e63bd927b1adc5ea4dc4d24cbd 🚀

--- index.9f338c82.js
+++
@@ -1,2 +0,0 @@
-const e=new MoviesApi,t=getRefs();t.form.addEventListener("submit",(function(a){a.preventDefault();const{elements:{searchQuery:n}}=a.currentTarget;t.gallery.innerHTML="",e.query=n.value,e.getSearchMovies().then((e=>{console.log(e.data),moviesMarkUp(e.data.results),createPagination(e.data.total_results,n)})),n.value=""})),onLoadPage();
-//# sourceMappingURL=index.9f338c82.js.map
