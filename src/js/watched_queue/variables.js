00f60fb978022579a1f2254a5e629376abe4db7d
Iryna <nebesna1808@gmail.com>
2023-04-17 19:19:14 +1100

    first commit

---
+++ src/js/watched_queue/variables.js
@@ -0,0 +1,16 @@
+const refs = {
+  
+  homeEl: document.querySelector('.header__link-home'),
+  collectionList: document.getElementById('.list-films-js'),
+  libraryEl: document.querySelector('.header__link-library'),
+  headerEl: document.querySelector('.header'),
+  searchEl: document.querySelector('.header__search-form'),
+  btnsEl: document.querySelector('.header__user-links-wrapper'),
+  watchedEl: document.querySelector('.js-watched'),
+  queueEl: document.querySelector('.js-queue'),
+  paginationElement: document.getElementById('pagination'),
+  homePagination: document.querySelector('.pagination__home'),
+  libraryPagination: document.querySelector('.pagination__library'),
+};
+
+export default refs;
