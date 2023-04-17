13d621f7dee5c89e1a7eda093126d232b4e0f0ef
Andrii Hashenko <hashenkoaa@gmail.com>
2023-04-17 21:51:51 +0300

    add refs

---
+++ src/js/refs.js
@@ -0,0 +1,28 @@
+export default function getRefs() {
+  return {
+    homeEl: document.querySelector('.header__link-home'),
+    libraryEl: document.querySelector('.header__link-library'),
+    headerEl: document.querySelector('.header'),
+    searchEl: document.querySelector('.header__search-form'),
+    btnsEl: document.querySelector('.header__user-links-wrapper'),
+    watchedEl: document.querySelector('.js-watched'),
+    queueEl: document.querySelector('.js-queue'),
+    galleryfilm: document.querySelector('.list-films-js');
+
+
+    //   примеры
+    
+  
+    //   btnAddWatched: document.querySelector(''),
+    //   btnAddQueue: document.querySelector(''),
+   
+    //   btnSectionInLibrary: document.querySelector(''),
+    //   homePage: document.querySelector(''),
+    //   headerByClass: document.querySelector(''),
+    //   headerByTeg: document.querySelector(''),
+    //   search: document.querySelector(''),
+    //   searchItem: document.querySelector(''),
+    //   searchInput: document.querySelector(''),
+    //   pagination: document.querySelector(''),
+  };
+}
