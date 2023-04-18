f1137ec93c98ed243c6338746b6a9e3dcf2b5a96
OlenkaPasko <lena_nechipurenko@ukr.net>
2023-04-18 21:01:41 +0300

    Update movie-search.js

--- src/js/movie-search.js
+++ src/js/movie-search.js
@@ -1,6 +1,5 @@
 import { getTrending } from '.src/.js/.api-service.js';
-//import { SEARCH_URL } from '.src/.js/.api.js';
-
+import { SEARCH_URL } from '.src/.js/.api.js';
 
 const searchForm = (document.querySelector('.header__search-form').innerHTML =
   ' ');
@@ -11,29 +10,23 @@ searchForm.addEventListener('submit', onSearchingMov);
 
 function onSearchingMov(event) {
   event.preventDefault();
-  getTrending.searchQuery = event.target.firstElementChild.value;
-  if (event.target.firstElementChild.value === ' ') {
+  getTrending = event.target.firstElementChild.value;
+  if (event.target.firstElementChild.value === ' ') 
     return;
-  }
 }
 //відображення фільмів
-  function fetchMoviesKeywords(keywords) {
-    return keywords.hits
-      .map(({ SEARCH_URL, searchForm, language }) => {
-        return `< div class="search-wrapper" >
+function fetchMoviesKeywords(keywords) {
+  return keywords.hits
+    .map(({ SEARCH_URL, searchForm, language }) => {
+      return `< div class="search-wrapper" >
       <form class="${searchForm}" action="${SEARCH_URL}">
         <div class="input-wrapper">
           <input
             class="header__input"
             type="${language}"
-            name="${card.name}"
+            name="${keywords.name}"
             placeholder="Movie search"
           />`;
-      })
-      .join('');
-  }
-
-
-
-
-
+    })
+    .join('');
+}
