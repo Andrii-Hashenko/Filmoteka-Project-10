9c9fa283e04ee500c5db68f15d1ac350dfb511e9
Andrii Hashenko <hashenkoaa@gmail.com>
2023-04-18 16:48:48 +0300

    changes refs

--- src/js/refs.js
+++ src/js/refs.js
@@ -1,23 +1,12 @@
 const refs = {
-  
-    homeEl: document.querySelector('.header__link-home'),
-    libraryEl: document.querySelector('.header__link-library'),
-    headerEl: document.querySelector('.header'),
-    searchEl: document.querySelector('.header__search-form'),
-    btnsEl: document.querySelector('.header__user-links-wrapper'),
-    watchedEl: document.querySelector('.js-watched'),
-    queueEl: document.querySelector('.js-queue'),
-    galleryfilm: document.querySelector('.list-films-js'),
-    libraryfilm: document.querySelector('#films-library'),
-
-    //   примеры
-
-    //   btnAddWatched: document.querySelector(''),
-    //   btnAddQueue: document.querySelector(''),
-
-    //   search: document.querySelector(''),
-    //   searchItem: document.querySelector(''),
-    //   searchInput: document.querySelector(''),
-    //   pagination: document.querySelector(''),
-  };
+  homeEl: document.querySelector('.header__link-home'),
+  libraryEl: document.querySelector('.header__link-library'),
+  headerEl: document.querySelector('.header'),
+  searchEl: document.querySelector('.header__search-form'),
+  btnsEl: document.querySelector('.header__user-links-wrapper'),
+  watchedEl: document.querySelector('.js-watched'),
+  queueEl: document.querySelector('.js-queue'),
+  galleryfilm: document.querySelector('#films-main'),
+  libraryfilm: document.querySelector('#films-library'),
+};
 export default refs;
