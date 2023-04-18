b5a36c201f02e7433c050bbe2926c6dc61b80b3c
Andrii Hashenko <113782851+Andrii-Hashenko@users.noreply.github.com>
2023-04-18 21:37:37 +0300

    Revert "library-nothing (#25)"
    This reverts commit 9866a9c2c933f0abb4d4dae3c6da6371eef3292f.

--- src/index.js
+++ src/index.js
@@ -3,12 +3,12 @@ import './sass/index.scss';
 import './js/api/api';
 import './js/api/api-service';
 import './js/gallery/render-film-cards';
-import './js/modal/film-modal';
-import './js/gallery/add-storege';
+import './js/modal/film-modal'
+// import './js/gallery/add-storege';
 import './js/gallery/formatGenres';
 import './js/gallery/galleryMarkupCards';
-import './js/gallery/libraryMarkupCards';
-import './js/gallery/render-film-storege';
+// import './js/gallery/libraryMarkupCards';
+// import './js/gallery/render-film-storege';
 import './js/gallery/genresList';
 // import './js/modal/team-modal';
 import './js/refs';
