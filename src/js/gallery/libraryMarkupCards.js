a7febfa3a0ff2ab675e13b8a8f9421a4727f2b09
Andrii Hashenko <hashenkoaa@gmail.com>
2023-04-18 23:11:39 +0300

    update footer

--- src/js/gallery/libraryMarkupCards.js
+++ src/js/gallery/libraryMarkupCards.js
@@ -1,5 +1,5 @@
 import { genresLibraryFormat } from './formatGenres';
-import no_poster from '../../images/no_poster/no_poster.wp.webp';
+import no_poster from '../../images/no_poster/nothing.wp.webp';
 
 function createLibraryMarkup(films) {
   return films
