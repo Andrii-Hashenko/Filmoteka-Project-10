db0a9f803c5ebb4ebefb63455913c3642e11972a
Andrii Hashenko <hashenkoaa@gmail.com>
2023-04-19 16:57:18 +0300

    fix

--- src/index.js
+++ src/index.js
@@ -3,14 +3,14 @@ import './sass/index.scss';
 import './js/api/api';
 import './js/api/api-service';
 import './js/refs';
-import './js/movie-search';
+// import './js/movie-search';
 import './js/gallery/render-film-cards';
 // import './js/modal/film-modal'
-// import './js/gallery/add-storege';
+import './js/gallery/add-storege';
 import './js/gallery/formatGenres';
 import './js/gallery/galleryMarkupCards';
 import './js/gallery/libraryMarkupCards';
-// import './js/gallery/render-film-storege';
+import './js/gallery/render-film-storege';
 import './js/gallery/genresList';
 // import './js/components/tui-pagination';
 // import './js/modal/team-modal';
