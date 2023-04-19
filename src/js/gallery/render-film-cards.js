5b61a9ec408ea339acf1ffed69b22ebb5c697c4f
Iryna <nebesna1808@gmail.com>
2023-04-19 18:38:32 +1100

    correct img

--- src/js/gallery/render-film-cards.js
+++ src/js/gallery/render-film-cards.js
@@ -3,9 +3,13 @@ import { createGalleryMarkup } from '../gallery/galleryMarkupCards';
 
 import { getMovieById } from '../api/api-service';
 
+import nothing from '../../images/no_poster/nothing.wp.webp';
+import { getMovieById } from '../api/api-service';
+import refs from '../refs';
+
 const modalEl = document.querySelector('.modal');
 const addWatchedBtn = document.querySelector('.add-to-watched');
-const libraryWatchedHeaderBtn = document.querySelector('.js-watched');
+const libraryWatchedBtn = document.querySelector('.js-watched');
 const galleryfilm = document.getElementById('films-main');
 const libraryfilm = document.getElementById('films-library');
 console.log(galleryfilm);
@@ -38,6 +42,7 @@ getTrending().then(data => {
 
     const movieTitleContainer = modalEl.querySelector('.modal_title');
     movieTitleContainer.textContent = movieInfo.original_title;
+    
   }));
 });
 
@@ -70,7 +75,7 @@ function checkIfWatched(movieId) {
   return watchedMoviesIds.includes(movieId);
 }
 
-libraryWatchedHeaderBtn.addEventListener('click', renderWatched);
+libraryWatchedBtn.addEventListener('click', renderWatched);
 
 async function renderWatched () {
     libraryfilm.innerHTML = '';
@@ -99,7 +104,12 @@ async function renderWatched () {
         movieTitleContainer.textContent = movieInfo.original_title;
       }))
     } else {
-      libraryfilm.innerHTML = '<h2> No movies watched </h2> ';
+      refs.libraryfilm.innerHTML = `
+      <li class="nothing">
+        <img src="${nothing}" alt="There's nothing to see here" />
+      </li>`;
+    refs.libraryfilm.classList.remove('films__container');
+    return;
     }
   };
 
@@ -112,16 +122,70 @@ function setWatchedIds () {
 }
 
 
+/*Library Queue*/
 
+addQueueBtn.addEventListener('click', addToQueue);
 
+function addToQueue() {
+  modalEl.style.display = 'none';
+  addToQueueLS(movieId);
 
-// import { getTrending } from '../api/api-service';
-// import { createGalleryMarkup } from '../gallery/galleryMarkupCards';
-// import refs from '../refs';
+  renderQueue();
+}
 
-// getTrending().then(data => {
-//   refs.galleryfilm.insertAdjacentHTML(
-//     'beforeend',
-//     createGalleryMarkup(data.results)
-//   );
-// });
+function addToQueueLS(movieId) {
+  if (checkIfQueue(movieId)) {
+    queueMoviesIds = queueMoviesIds.filter(id => id !== movieId);
+  } else {
+    queueMoviesIds.push(movieId);
+  }
+
+  localStorage.setItem('queue', JSON.stringify(queueMoviesIds));
+}
+
+function checkIfQueue(movieId) {
+  return queueMoviesIds.includes(movieId);
+}
+
+libraryQueueHeaderBtn.addEventListener('click', renderQueue);
+
+async function renderQueue() {
+  libraryfilm.innerHTML = '';
+
+  if (queueMoviesIds.length) {
+    try {
+      for (const id of queueMoviesIds) {
+        const movie = await getMovieById(id);
+        queueMoviesInfo.push(movie);
+      }
+
+      const layout = createGalleryMarkup(queueMoviesInfo);
+      libraryfilm.insertAdjacentHTML('beforeend', layout);
+    } catch (error) {
+      // error handling
+    }
+
+    const allCards = libraryfilm.querySelectorAll('.film__card');
+    allCards.forEach(card =>
+      card.addEventListener('click', async () => {
+        modalEl.style.display = 'block';
+        movieId = card.dataset.film;
+
+        const movieInfo = await getMovieById(movieId);
+
+        const movieTitleContainer = modalEl.querySelector('.modal_title');
+        movieTitleContainer.textContent = movieInfo.original_title;
+      })
+    );
+  } else {
+    libraryfilm.innerHTML = '<h2> No movies in queue </h2> ';
+  }
+}
+
+function setQueueIds() {
+  if (localStorage.getItem('queue')) {
+    return JSON.parse(localStorage.getItem('queue'));
+  } else {
+    return [];
+  }
+}
