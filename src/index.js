c3f198e4ed38bd3ae16fe1815be9253e01c00b4b
Andrii Hashenko <hashenkoaa@gmail.com>
2023-04-19 14:04:14 +0300

    changes code

--- src/index.js
+++ src/index.js
@@ -15,24 +15,3 @@ import './js/gallery/genresList';
 // import './js/modal/team-modal';
 
 
-// const movies = new MoviesApi();
-// const refs = getRefs();
-
-// refs.form.addEventListener('submit', onFormSubmit);
-
-// function onFormSubmit(e) {
-//   e.preventDefault();
-//   const {
-//     elements: { searchQuery },
-//   } = e.currentTarget;
-//   refs.gallery.innerHTML = '';
-//   movies.query = searchQuery.value;
-//   movies.getSearchMovies().then(response => {
-//     console.log(response.data);
-//     moviesMarkUp(response.data.results);
-//     createPagination(response.data.total_results, searchQuery);
-//   });
-//   searchQuery.value = '';
-// }
-
-// onLoadPage();
