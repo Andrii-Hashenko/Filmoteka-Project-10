d058207d9bd6f418aaf094d0b58d52cc21c763b8
Sergiy Kushnir <114180707+Sergiy5@users.noreply.github.com>
2023-04-19 16:50:57 +0300

    Set storage (#33)
    * Підключив storage.
    
    Змінив файли: index.js - додав імпорт set-storage;
    змінив паршал модалки - додав атрибут "name" до кнопок,
    змінив файл render-film-cards- шмпортівав вункцію set-storage, підключив слухача на контейнер (div) кнопок.
    
    * Видалив зайве з функціаналу стореджа
    
    ---------
    
    Co-authored-by: Andrii Hashenko <113782851+Andrii-Hashenko@users.noreply.github.com>

--- src/js/gallery/render-film-cards.js
+++ src/js/gallery/render-film-cards.js
@@ -2,16 +2,15 @@ import { getTrending } from '../api/api-service';
 import { createGalleryMarkup } from '../gallery/galleryMarkupCards';
 import { genresGalleryFormatModal } from './formatGenres';
 import { getMovieById } from '../api/api-service';
+import { addMovieToStorage } from '../storage/set-storage';
 
 const modalEl = document.querySelector('.modal');
-const addWatchedBtn = document.querySelector('.add-to-watched');
-const libraryWatchedHeaderBtn = document.querySelector('.js-watched');
+const libraryWatchedHeaderBtn = document.querySelector('.header__link-library');
 const galleryfilm = document.getElementById('films-main');
 const libraryfilm = document.getElementById('films-library');
-console.log(galleryfilm); // index.html 
-console.log(libraryfilm); // library.html
 
-console.log(libraryWatchedHeaderBtn, '!!!');
+const btnStorage = document.querySelector('.modal_buttons');
+
 
 let movieId = null;
 
@@ -19,54 +18,44 @@ let watchedMoviesIds = setWatchedIds();
 
 let watchedMoviesInfo = [];
 
-console.log(watchedMoviesIds);
 
-if (galleryfilm) {
-  getTrending().then(data => {
-    console.log(galleryfilm, 'gallery film');
-    galleryfilm.insertAdjacentHTML(
-      'beforeend',
-      createGalleryMarkup(data.results),
-    );
-  })
-  .then(() => {
-    const allCards = document.querySelectorAll('.film__card');
-  
-    allCards.forEach(card => card.addEventListener('click', async () => {
-      console.log(card.dataset.film);
-      modalEl.style.display = 'block';
-      document.body.classList.add('stop-scrolling');
-      movieId = card.dataset.film;
-      const movieInfo = await getMovieById(movieId);
-  
-      const movieTitleContainer = modalEl.querySelector('.modal_title');
-      movieTitleContainer.textContent = movieInfo.original_title;
-  
-      modalEl.querySelector('.modal_image').src =
-        `https://image.tmdb.org/t/p/w500/${movieInfo.poster_path}`;
-      
-      const movieVote = modalEl.querySelector('.vote');
-      movieVote.textContent = `${movieInfo.vote_average} / ${movieInfo.vote_count}`;
-  
-      const moviePopularity = modalEl.querySelector('.popularity');
-      moviePopularity.textContent = movieInfo.popularity;
-  
-      const movieOriginalTitle = modalEl.querySelector('.original-title');
-      movieOriginalTitle.textContent = movieInfo.original_title;
-      
-  
-      const genres = genresGalleryFormatModal(movieInfo.genres);
-  
-      const movieGenres = modalEl.querySelector('.genre');
-      movieGenres.textContent = genres;
-  
-      const movieOverview = modalEl.querySelector('.modal_description');
-      movieOverview.textContent = movieInfo.overview;
-  
-      // console.log(genresGalleryFormat(movieInfo.genre_ids));
-    }));
-  });
-}
+
+getTrending().then(data => {
+  galleryfilm.insertAdjacentHTML(
+    'beforeend',
+    createGalleryMarkup(data.results)
+  );
+})
+.then(() => {
+  const allCards = document.querySelectorAll('.film__card');
+
+  allCards.forEach(card => card.addEventListener('click', async () => {
+    modalEl.style.display = 'block';
+    document.body.classList.add('stop-scrolling');
+    movieId = card.dataset.film;
+    const movieInfo = await getMovieById(movieId);
+
+    const movieTitleContainer = modalEl.querySelector('.modal_title');
+    movieTitleContainer.textContent = movieInfo.original_title;
+
+    modalEl.querySelector('.modal_image').src =
+      `https://image.tmdb.org/t/p/w500/${movieInfo.poster_path}`;
+    
+    const movieVote = modalEl.querySelector('.vote');
+    movieVote.textContent = `${movieInfo.vote_average} / ${movieInfo.vote_count}`;
+
+    const moviePopularity = modalEl.querySelector('.popularity');
+    moviePopularity.textContent = movieInfo.popularity;
+
+    const movieOriginalTitle = modalEl.querySelector('.original-title');
+    movieOriginalTitle.textContent = movieInfo.original_title;
+    
+    const genres = genresGalleryFormatModal(movieInfo.genre_ids);
+    const movieGenres = modalEl.querySelector('.genre');
+    movieGenres.textContent = genres;
+
+    const movieOverview = modalEl.querySelector('.modal_description');
+    movieOverview.textContent = movieInfo.overview;
 
 
 
@@ -94,95 +83,92 @@ window.addEventListener('keydown', e => {
   }
 });
 
-/*Library Watched*/
+/*Library*/
 
-addWatchedBtn.addEventListener('click', addToWatched);
 
-function addToWatched() {
-  modalEl.style.display = 'none';
-  addToWatchedLS(movieId);
+btnStorage.addEventListener('click', addToStorage);
 
-  renderWatched();
+
+function addToStorage(e) {
+  const nameEvt = e.target.name;
+addMovieToStorage(nameEvt, movieId);
 };
 
 
-function addToWatchedLS(movieId) {
-  if (checkIfWatched(movieId)) {
-    watchedMoviesIds = watchedMoviesIds.filter(id => id !== movieId);
+// function addToWatchedQueueLS(movieId) {
+//   if (checkIfWatched(movieId)) {
+//     watchedMoviesIds = watchedMoviesIds.filter(id => id !== movieId);
+    
+//   } else {
+//     watchedMoviesIds.push(movieId);
+//   }
+
+//   localStorage.setItem('watched', JSON.stringify(watchedMoviesIds));
+// }
+
+// function checkIfWatched(movieId) {
+//   return watchedMoviesIds.includes(movieId);
+// }
+
+// libraryWatchedHeaderBtn.addEventListener('click', renderWatched);
+
+// async function renderWatched () {
+//     libraryfilm.innerHTML = '';
+//   console.log(watchedMoviesIds);
+//     if(watchedMoviesIds.length) {
+//       try {
+//         for (const id of watchedMoviesIds) {
+//           const movie = await getMovieById(id);
+//           watchedMoviesInfo.push(movie);
+//         }
+  
+//         const layout = createGalleryMarkup(watchedMoviesInfo);
+//         libraryfilm.insertAdjacentHTML('beforeend', layout);
+//       } catch (error) {
+//         // error handling      
+//       }
+  
+//       const allCards = libraryfilm.querySelectorAll('.film__card');
+//       allCards.forEach(card => card.addEventListener('click', async () => {
+//         modalEl.style.display = 'block';
+//         movieId = card.dataset.film;
+  
+//         const movieInfo = await getMovieById(movieId);
+  
+//        const movieTitleContainer = modalEl.querySelector('.modal_title');
+//        movieTitleContainer.textContent = movieInfo.original_title;
+
+//        modalEl.querySelector(
+//          '.modal_image'
+//        ).src = `https://image.tmdb.org/t/p/w500/${movieInfo.poster_path}`;
+
+//        const movieVote = modalEl.querySelector('.vote');
+//        movieVote.textContent = `${movieInfo.vote_average} / ${movieInfo.vote_count}`;
+
+//        const moviePopularity = modalEl.querySelector('.popularity');
+//        moviePopularity.textContent = movieInfo.popularity;
+
+//        const movieOriginalTitle = modalEl.querySelector('.original-title');
+//        movieOriginalTitle.textContent = movieInfo.original_title;
+
+//        const genres = genresGalleryFormatModal(movieInfo.genre_ids);
+//        const movieGenres = modalEl.querySelector('.genre');
+//        movieGenres.textContent = genres;
+
+//        const movieOverview = modalEl.querySelector('.modal_description');
+//        movieOverview.textContent = movieInfo.overview;
+//       }))
+//     } else {
+//       libraryfilm.innerHTML = '<h2> No movies watched </h2> ';
+//     }
+//   };
+
+function setWatchedIds () {
+  if (localStorage.getItem('watched')) {
+    return JSON.parse(localStorage.getItem('watched'));
   } else {
-    watchedMoviesIds.push(movieId);
-  }
+    return [];
 
-  localStorage.setItem('watched', JSON.stringify(watchedMoviesIds));
-}
-
-function checkIfWatched(movieId) {
-  return watchedMoviesIds.includes(movieId);
-}
-
-if (libraryfilm) {
-  libraryWatchedHeaderBtn.addEventListener('click', renderWatched);
-  
-  async function renderWatched () {
-      libraryfilm.innerHTML = '';
-
-      if(watchedMoviesIds.length) {
-        try {
-          for (const id of watchedMoviesIds) {
-            const movie = await getMovieById(id);
-            watchedMoviesInfo.push(movie);
-          }
-    
-          const layout = createGalleryMarkup(watchedMoviesInfo);
-          libraryfilm.insertAdjacentHTML('beforeend', layout);
-        } catch (error) {
-          // error handling      
-        }
-    
-        const allCards = libraryfilm.querySelectorAll('.film__card');
-        allCards.forEach(card => card.addEventListener('click', async () => {
-          modalEl.style.display = 'block';
-          movieId = card.dataset.film;
-    
-          const movieInfo = await getMovieById(movieId);
-    
-         const movieTitleContainer = modalEl.querySelector('.modal_title');
-         movieTitleContainer.textContent = movieInfo.original_title;
-  
-         modalEl.querySelector(
-           '.modal_image'
-         ).src = `https://image.tmdb.org/t/p/w500/${movieInfo.poster_path}`;
-  
-         const movieVote = modalEl.querySelector('.vote');
-         movieVote.textContent = `${movieInfo.vote_average} / ${movieInfo.vote_count}`;
-  
-         const moviePopularity = modalEl.querySelector('.popularity');
-         moviePopularity.textContent = movieInfo.popularity;
-  
-         const movieOriginalTitle = modalEl.querySelector('.original-title');
-         movieOriginalTitle.textContent = movieInfo.original_title;
-  
-         const genres = genresGalleryFormatModal(movieInfo.genre_ids);
-         const movieGenres = modalEl.querySelector('.genre');
-         movieGenres.textContent = genres;
-  
-         const movieOverview = modalEl.querySelector('.modal_description');
-         movieOverview.textContent = movieInfo.overview;
-        }))
-      } else {
-        libraryfilm.innerHTML = '<h2> No movies watched </h2> ';
-      }
-    };
-
-    renderWatched();
-  }
-  
-  function setWatchedIds () {
-    if (localStorage.getItem('watched')) {
-      return JSON.parse(localStorage.getItem('watched'));
-    } else {
-      return [];
-    }
   }
 
 // import { getTrending } from '../api/api-service';
